d83cf398571fbc5cbb854d9b95ab10a5f62c7c7f
letyresina <leticiaresina@gmail.com>
2024-04-18 13:35:20 -0300

    subindo alguns comandos

--- src/br/com/fiap/ManipulaString.java
+++ src/br/com/fiap/ManipulaString.java
@@ -1,10 +1,32 @@
 package br.com.fiap;
 
+import javax.swing.JOptionPane;
+
 public class ManipulaString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String frase = "O rato roeu a roupa do rei de roma";
+		JOptionPane.showMessageDialog(null, frase);
+		
+		// exibindo a quantidade de caracteres
+		int qtdChar = frase.length();
+		JOptionPane.showMessageDialog(null, qtdChar);
+		
+		// exibindo tudo em maiusculo
+		String maiuscula = frase.toUpperCase();
+		JOptionPane.showMessageDialog(null, maiuscula);
+		
+		// exibindo tudo em minusculo
+		String minusculo = frase.toLowerCase();
+		JOptionPane.showMessageDialog(null, minusculo);
+		
+		// extraindo a palavra roma e exibindo
+		String palavra = frase.substring(30,34);
+		JOptionPane.showMessageDialog(null, palavra);
+		
+		// substituindo palavras
+		String frase2 = frase.replace("roupa", "parede");
+		JOptionPane.showMessageDialog(null, frase2);
 	}
 
 }
